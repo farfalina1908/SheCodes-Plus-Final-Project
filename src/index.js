89c4414cbb4c46eb5fdd6e11af0d1b6147073d24
Elena Berezina <farfalina1908@gmail.com>
2023-01-27 13:15:40 +0200

    Updated forecast

--- src/index.js
+++ src/index.js
@@ -25,6 +25,52 @@ let currentDate = document.querySelector("#currentDay");
 let currentTime = new Date();
 currentDate.innerHTML = showDate(currentTime);
 
+function formatDay(timestamp) {
+   let date = new Date(timestamp * 1000);
+   let day = date.getDay();
+   
+   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+   return days[day];
+   }
+
+function displayForecast(response) {
+   let forecast = response.data.daily;
+   let forecastElement = document.querySelector("#forecast");
+   let forecastHTML = `<div class="row">`;
+  
+   forecast.forEach(function(forecastDay, index) { 
+       if (index < 6) {
+   forecastHTML = forecastHTML + `
+   
+   <div class="col-2">
+       <div class="weather-forecast-date">
+       ${formatDay(forecastDay.dt)}
+   </div>
+   <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="80">
+   <div class="weather-forecast-temperature">
+       <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}°</span>
+   <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}°</span> 
+</div>
+</div>
+`;}
+       }
+)
+       
+  
+   forecastHTML = forecastHTML + `</div>`;
+   forecastElement.innerHTML = forecastHTML;
+
+   
+}
+
+function getForecast(coordinates) {
+
+let apiKey = "c119ffef35b7245a5e03b6e5724ae961";
+let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayForecast);
+}
+
+
 
 function showTemp(response) {
    console.log(response.data);
@@ -43,6 +89,8 @@ function showTemp(response) {
    windElement.innerHTML = Math.round(response.data.wind.speed);
    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
    iconElement.setAttribute("alt", response.data.weather[0].description);
+
+   getForecast(response.data.coord);
 }
 
 
@@ -91,6 +139,7 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", changeToCelsius);
 
 search("Dnipro");
+displayForecast();
 
 
 function handlePosition(position) {
